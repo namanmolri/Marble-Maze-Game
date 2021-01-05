fd1df9e4d57d9d135edbe0b16522c2525f519acb
Sam Soon <ssoon@umd.edu>
2021-01-05 15:37:19 -0500

    first

--- MarbleMaze/app/src/main/java/com/example/marblemaze/MainActivity.kt
+++ MarbleMaze/app/src/main/java/com/example/marblemaze/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.marblemaze
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start)
+        val optionsButton = findViewById<Button>(R.id.options)
+        val quitButton = findViewById<Button>(R.id.quit)
+
+        quitButton.setOnClickListener(){
+            finishAndRemoveTask()
+        }
     }
 }
